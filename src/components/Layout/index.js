c561a16dc70efdc6a5f35e43b4e360dfd6e62e2e
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-03-05 08:58:40 -0300

    Add GlobalStyles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,21 +3,21 @@ import PropTypes from "prop-types"
 import Profile from "../Profile"
 
 import styled from "styled-components"
+import GlobalStyles from "../../styles/global"
 
 const LayoutWrapper = styled.section`
   display: flex;
 `
-
 const LayoutMain = styled.main`
   background: #16202c;
   min-height: 100vh;
   padding: 0 3.75rem 0 20rem;
   width: 100%;
 `
-
 const Layout = ({ children }) => {
   return (
     <LayoutWrapper>
+      <GlobalStyles />
       <aside>
         <Profile />
       </aside>
@@ -25,9 +25,7 @@ const Layout = ({ children }) => {
     </LayoutWrapper>
   )
 }
-
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
-
 export default Layout
