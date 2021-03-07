4f9b66bed97ec211e1209913d360adb74ccc28f1
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-03-07 07:56:29 -0300

    Create SocialLinks Content

---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,15 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { YouTube } from "@styled-icons/boxicons-logos/Youtube"
+import { Tumblr } from "@styled-icons/boxicons-logos/Tumblr"
+import { Instagram } from "@styled-icons/boxicons-logos/Tumblr"
+
+const Icons = {
+    Github,
+    Twitter,
+    Youtube,
+    Tumblr,
+    Instagram,
+}
+
+export default Icons
