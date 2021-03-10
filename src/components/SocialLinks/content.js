247c8e879a4b290776cc5847c60c5802ff0c5248
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-03-10 07:24:43 -0300

    Create SocialLinks component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+      label: "Github",
+      url: `https://github.com/victorhugoalmeid`,
+    },
+    {
+      label: "Twitter",
+      url: `https://twitter.com/VlCTORHUG0`,
+    },
+    {
+      label: "Youtube",
+      url: `https://www.youtube.com/channel/UCA2gdXfSoPmHG1ynV0BZPbg?view_as=subscriber`,
+    },
+    {
+      label: "Instagram",
+      url: `https://www.instagram.com/ubernmensch/`,
+    },
+    {
+      label: "Facebook",
+      url: `https://www.facebook.com/vtorugo/`,
+    },
+  ]
+  
+  export default links
--- src/components/SocialLinks/icons.js
+++ src/components/SocialLinks/icons.js
@@ -1,15 +1,15 @@
-import { Github } from "@styled-icons/boxicons-logos/Github"
-import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
-import { YouTube } from "@styled-icons/boxicons-logos/Youtube"
-import { Tumblr } from "@styled-icons/boxicons-logos/Tumblr"
-import { Instagram } from "@styled-icons/boxicons-logos/Tumblr"
+import { Github } from '@styled-icons/boxicons-logos/Github'
+import { Twitter } from '@styled-icons/boxicons-logos/Twitter'
+import { Youtube } from '@styled-icons/boxicons-logos/Youtube'
+import { Facebook } from '@styled-icons/boxicons-logos/Facebook'
+import { Instagram } from '@styled-icons/boxicons-logos/Instagram'
 
 const Icons = {
-    Github,
-    Twitter,
-    Youtube,
-    Tumblr,
-    Instagram,
+  Github,
+  Twitter,
+  Youtube,
+  Facebook,
+  Instagram,
 }
 
 export default Icons
