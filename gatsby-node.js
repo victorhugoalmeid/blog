58a9498c414dadeeb55f28603355b71f618baded
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-03-11 14:52:08 -0300

    Add `createPages` method

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 
@@ -20,4 +21,33 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       })
     }
   }
-  
+
+// To create the posts pages
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    return graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            slug: node.fields.slug,
+          },
+        })
+      })
+    })
+  }
