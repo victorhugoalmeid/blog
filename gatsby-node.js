ea967dca2c2d2b337b51ed79c8d9f27351bdcf26
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-03-22 16:42:41 -0300

    Create pagination logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,23 +22,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     }
   }
 
-// To create the posts pages
 exports.createPages = ({ graphql, actions }) => {
     const { createPage } = actions
     return graphql(`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-              }
+    {
+      allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+        edges {
+          node {
+            fields{
+              slug
             }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
           }
         }
       }
+    }
     `).then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      const posts = result.data.allMarkdownRemark.edges
+
+      posts.forEach(({ node }) => {
         createPage({
           path: node.fields.slug,
           component: path.resolve(`./src/templates/blog-post.js`),
@@ -49,5 +58,21 @@ exports.createPages = ({ graphql, actions }) => {
           },
         })
       })
+
+      const postsPerPage = 6
+      const numPages = Math.ceil(posts.length / postsPerPage)
+  
+      Array.from({ length: numPages }).forEach((_, index) => {
+        createPage({
+          path: index === 0 ? `/` : `/page/${index + 1}`,
+          component: path.resolve(`./src/templates/blog-list.js`),
+          context: {
+            limit: postsPerPage,
+            skip: index * postsPerPage,
+            numPages,
+            currentPage: index + 1,
+          },
+        })
+      })
     })
-  }
+  }          
