e9c03d87318abec0a82f1b65744b1180a4ad5551
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-04-01 06:18:50 -0300

    att plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    //needs to be frist to work with gatsby-remark-images
+    // needs to be the first to work with gatsby-remark-images
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -35,19 +35,19 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: "gatsby-remark-relativ-images-v2",
+            resolve: "gatsby-remark-relative-images",
             options: {
-              name: "uploads"
-            }
+              name: "uploads",
+            },
           },
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth:960,
-              linkImagesToOriginal: false
+              maxWidth: 960,
+              linkImagesToOriginal: false,
             },
           },
-          `gatsby-remark-lazy-load`,       
+          `gatsby-remark-lazy-load`,
         ],
       },
     },
