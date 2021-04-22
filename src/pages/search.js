73b97c6cda865a7e8adef15ef36a301c47c2d380
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-04-22 03:32:29 -0300

    Add css variables

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/Layout"
-import SEO from "../components/seo"
-import Search from "../components/Search"
+import Layout from '../components/Layout/'
+import SEO from '../components/seo'
+import Search from '../components/Search'
 
-const SearchPage = () => (
-  <Layout>
-    <SEO title="Search" />
-    <Search />
-  </Layout>
-)
+const algolia = {
+  appId: process.env.GATSBY_ALGOLIA_APP_ID,
+  searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_ONLY_KEY,
+  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME
+}
+
+const SearchPage = props => {
+  return (
+    <Layout>
+      <SEO
+        title="Search"
+        description="Vai lá, não tenha medo. Busque por posts novos e bem antigos."
+      />
+      <Search algolia={algolia} />
+    </Layout>
+  )
+}
 
 export default SearchPage
