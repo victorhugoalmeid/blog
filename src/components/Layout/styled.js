fe9ae5b7f107ee7948f0d81954e237a53a52b1e4
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-04-22 22:42:28 -0300

    Add grid styles

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -1,12 +1,15 @@
 import styled from "styled-components"
+
 export const LayoutWrapper = styled.section`
   display: flex;
 `
+
 export const LayoutMain = styled.main`
   background: var(--background);
   min-height: 100vh;
   padding: 0 3.75rem 0 20rem;
   width: 100%;
+  
   body#grid & {
     grid-template-areas:
       "posts"
--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -6,6 +6,8 @@ import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 import Pagination from '../components/Pagination'
 
+import * as S from '../components/ListWrapper/styled'
+
 const BlogList = props => {
   const postList = props.data.allMarkdownRemark.edges
 
@@ -18,6 +20,7 @@ const BlogList = props => {
   return (
     <Layout>
       <SEO title="Home" />
+      <S.ListWrapper>
       {postList.map(
         ({
           node: {
@@ -37,6 +40,7 @@ const BlogList = props => {
           />
         )
       )}
+      </S.ListWrapper>
 
       <Pagination
         isFirst={isFirst}
