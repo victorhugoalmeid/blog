e7f2be1dc03931b9d0e8f5746970412169293a6e
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-04-30 05:30:36 -0300

    Add gatsby-plugin-image e update gatsby-image use

--- src/components/Avatar/styled.js
+++ src/components/Avatar/styled.js
@@ -1,15 +1,14 @@
 import styled from "styled-components"
 import media from "styled-media-query"
-import Img from "gatsby-image"
+import { GatsbyImage as Img } from "gatsby-plugin-image"
 
 export const AvatarWrapper = styled(Img)`
   border-radius: 50%;
   height: 3.75rem;
   margin: auto;
   width: 3.75rem;
-
   ${media.lessThan("large")`
-  height: 1.875rem;
-  width: 1.875rem;
+    height: 1.875rem;
+    width: 1.875rem;
   `}
 `
