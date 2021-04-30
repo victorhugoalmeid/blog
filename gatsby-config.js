410572c610faf45e1d49a33d667be8130b25223d
Victor Hugo Almeida <victorhugoalmeid@gmail.com>
2021-04-30 02:18:51 -0300

    Configure manifest, sitemap, seo, pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,11 +74,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Victor Hugo Almeida`,
+        short_name: `Victor Hugo`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -86,6 +86,6 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
